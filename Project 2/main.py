854b29a4f288e00fe1977b35149b133abc60e3d0
Nick Alvarez <nickalvarez189@gmail.com>
2021-03-17 09:53:02 -0700

    Add support for dbutils

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,66 +1,53 @@
 # Nick Alvarez, CS 657, PA2, Spring 21
 # Python 3.7+ required.
 
+# Main driver file
+
 import os
 import shlex
 import subprocess
+import dbutils
 
 workingDB = None
 UserQuery = None
 TableList = [None]
 
-def inputCleaner(wordToRemove): # Removes ; and command
-  query = UserQuery.replace(";", "")
-  return query.replace(wordToRemove, "")
-
-def databaseExistenceCheck(db): # Checks if database exists
-  if db in subprocess.run(['ls', '|', 'grep', db], capture_output=True, text=True).stdout:
-    return 1
-  else:
-    return 0
-
-def tableExistenceCheck(t): # Checks if table exists
-  if t in subprocess.run(['ls', workingDB,  '|', 'grep', t], capture_output=True, text=True).stdout:
-    return 1
-  else:
-    return 0
-
-while (UserQuery.lower() != ".EXIT"):
+while (UserQuery != ".EXIT"):
   UserQuery = input("nickQL> ")
   if (';' not in UserQuery and UserQuery != ".EXIT"): # Invalid command
     print("Commands must end with ';'")
   
   # Creates database
-  elif ("CREATE DATABASE" in UserQuery.lower()):
-    dbName = inputCleaner("CREATE DATABASE ")
-    if databaseExistenceCheck(dbName) == 0:
+  elif ("CREATE DATABASE" in UserQuery.upper()):
+    dbName = dbutils.inputCleaner("CREATE DATABASE ", UserQuery)
+    if dbutils.databaseExistenceCheck(dbName) == 0:
       os.system(f'mkdir {dbName}')
       print(f"Created database {dbName}.")
     else:
       print(f"Could not create database {dbName} because it already exists.")
   
   # Deletes database
-  elif ("DROP DATABASE" in UserQuery.lower()):
-    dbName = inputCleaner("DROP DATABASE ")
-    if databaseExistenceCheck(dbName):
+  elif ("DROP DATABASE" in UserQuery.upper()):
+    dbName = dbutils.inputCleaner("DROP DATABASE ", UserQuery)
+    if dbutils.databaseExistenceCheck(dbName):
       os.system(f'rm -r {dbName}')
       print(f"Removed database {dbName}.")
     else:
       print(f"Could not remove database {dbName} because it does not exist.")
   
   # Sets currently active database
-  elif ("USE" in UserQuery.lower()):
-    workingDB = inputCleaner("USE ")
+  elif ("USE" in UserQuery.upper()):
+    workingDB = dbutils.inputCleaner("USE ", UserQuery)
     #os.system('cd ' + workingDB)
-    if databaseExistenceCheck(workingDB):
+    if dbutils.databaseExistenceCheck(workingDB):
       print(f"Using database {workingDB}.")
     else:
       print(f"Could not use database {workingDB} because it does not exist.")
 
   # Creates a table with specified name and attributes
-  elif ("CREATE TABLE" in UserQuery.lower()):
+  elif ("CREATE TABLE" in UserQuery.upper()):
     # Splits input into separate strings
-    tInput = inputCleaner("CREATE TABLE ")
+    tInput = dbutils.inputCleaner("CREATE TABLE ", UserQuery)
     tName = tInput.split()[0] # Grabs table name
     tRest = tInput.replace(tName, "")
     tAttrs0 = tRest[2:] # Leaves only string with attributes
@@ -68,7 +55,7 @@ while (UserQuery.lower() != ".EXIT"):
     tAttrs = tAttrs1.split(",") # Creates list from attributes
 
     if (workingDB != None):
-      if tableExistenceCheck(tName) == 0:
+      if dbutils.tableExistenceCheck(tName, workingDB) == 0:
         os.system(f'touch {workingDB}/{tName}.txt')
         filename = workingDB + '/' + tName + '.txt'
         f = open(filename, 'w')
@@ -81,10 +68,10 @@ while (UserQuery.lower() != ".EXIT"):
       print("Please specify which database to use.")
 
   # Deletes table
-  elif ("DROP TABLE" in UserQuery.lower()):
-    tName = inputCleaner("DROP TABLE ")
+  elif ("DROP TABLE" in UserQuery.upper()):
+    tName = dbutils.inputCleaner("DROP TABLE ", UserQuery)
     if (workingDB != None):
-      if tableExistenceCheck(tName):
+      if dbutils.tableExistenceCheck(tName, workingDB):
         os.system(f'rm {workingDB}/{tName}.txt')
         print(f"Removed table {tName} from database {workingDB}.")
       else:
@@ -93,12 +80,12 @@ while (UserQuery.lower() != ".EXIT"):
       print("Please specify which database to use.")
   
   # Returns table elements as specified
-  elif ("SELECT *" in UserQuery.lower()):
-    selection = inputCleaner("SELECT * FROM ")
+  elif ("SELECT *" in UserQuery.upper()):
+    selection = dbutils.inputCleaner("SELECT * FROM ", UserQuery)
     #cmd = shlex.split(f"cat {workingDB}/{selection}.txt")
     #subprocess.Popen(cmd)
     if workingDB != None:
-      if tableExistenceCheck(selection):
+      if dbutils.tableExistenceCheck(selection, workingDB):
         f = open(f'{workingDB}/{selection}.txt', 'r')
         print(f.read())
         f.close()
@@ -108,8 +95,8 @@ while (UserQuery.lower() != ".EXIT"):
       print("Please specify which database to use.")
 
   # Modifies table by adding attribute
-  elif ("ALTER TABLE" in UserQuery.lower()):
-    alter = inputCleaner("ALTER TABLE ")
+  elif ("ALTER TABLE" in UserQuery.upper()):
+    alter = dbutils.inputCleaner("ALTER TABLE ", UserQuery)
     tName = alter.split()[0] # Grabs table name
     alterCmd = alter.split()[1] # Grabs command (ADD, etc)
     alterRest1 = alter.replace(tName, "")
@@ -117,7 +104,7 @@ while (UserQuery.lower() != ".EXIT"):
     newAttr = alterRest2[2:] # May have issue with leading parentheses
 
     if workingDB != None:
-      if tableExistenceCheck(tName):
+      if dbutils.tableExistenceCheck(tName, workingDB):
         f = open(f'{workingDB}/{tName}.txt', 'a')
         f.write(f" | {newAttr}") # Appends attribute to file with pipe delimiter
         f.close()
@@ -129,6 +116,6 @@ while (UserQuery.lower() != ".EXIT"):
   
   # Testing purposes, deletes databases to start fresh
   #elif ("DEL" in UserQuery):
-  #  os.system('rm -r db_1 db_2')
+  #  os.system('rm -r CS457_PA2')
 
 quit()
